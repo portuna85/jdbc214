503d527ef1eb72e622c579118d8672caf274d3ab
portuna85 <portuna85@gmail.com>
2022-04-24 14:43:07 +0900

    Spring DB Update

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,10 +2,15 @@ package hello.jdbc.repository;
 
 import hello.jdbc.Repository.MemberRepositoryV0;
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
 public class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -13,7 +18,13 @@ public class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         // save
-        Member member = new Member("memberV0", 10000);
+        Member member = new Member("memberV2", 10000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember = {}" , findMember);
+
+        assertThat(findMember).isEqualTo(member);
     }
 }
