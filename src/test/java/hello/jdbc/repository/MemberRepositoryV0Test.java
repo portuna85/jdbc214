3602d858fd284a67aa0fd7854d7d7406e2e11236
portuna85 <portuna85@gmail.com>
2022-04-24 14:03:15 +0900

    Spring DB Update

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,4 +1,19 @@
 package hello.jdbc.repository;
 
+import hello.jdbc.Repository.MemberRepositoryV0;
+import hello.jdbc.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 public class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        // save
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+    }
 }
