d9854fcc4ed025e2c7851de0c92f0ebd4e805772
portuna85 <portuna85@gmail.com>
2022-04-26 15:56:25 +0900

    Spring DB Update

--- src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -9,8 +9,7 @@ import java.sql.SQLException;
 @RequiredArgsConstructor
 public class MemberServiceV1 {
     private final MemberRepositoryV1 memberRepository;
-    public void accountTransfer(String fromId, String toId, int money) throws
-            SQLException {
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
         memberRepository.update(fromId, fromMember.getMoney() - money);
